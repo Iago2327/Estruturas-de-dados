871aa0907f9def1dcc697cffedd6a25ad5275c6d
Iago2327 <iagoalmeida@alunos.fho.edu.br>
2022-08-14 16:12:16 -0300

    apenas um teste

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
 using namespace std;
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
 
 int media(int v[],int tamanho){
-    int soma =0
+    int soma =0;
     for(int i=0;i<tamanho;i++){
         soma +=v[i];
     }
@@ -13,14 +11,10 @@ int media(int v[],int tamanho){
 }
 int main()
 {
-   
-    
     int a[]= {1,2,3};
     int b[]= {2,3,4,6,8,0};
-    cout<<"Madia de a= "<<media(a,3)<<endl;
-    cout<<"Madia de b= "<<media(a,6)<<endl;
-    return 0
-    
+    cout<<"Media de a= "<<media(a,3)<<endl;
+    cout<<"Media de b= "<<media(b,6)<<endl;
     return 0;
 }
 
